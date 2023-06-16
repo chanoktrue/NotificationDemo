1484f2bdb09681d158bb683f0aa1832fb73c1c0b
Thongchai Subsaidee <chanoktrue@me.com>
2023-06-16 13:13:13 +0700

    Notify demo

---
+++ NotificationDemo/NotificationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationDemoApp.swift
+//  NotificationDemo
+//
+//  Created by Thongchai Subsaidee on 16/6/23.
+//
+
+import SwiftUI
+
+@main
+struct NotificationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
